a8572f2e21f14e56735a93e4b51838680d68b10d
mattmarietta <mattmarietta24@gmail.com>
2025-03-23 09:11:19 -0700

    Fixed clickable link for each card

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 
 const Card = ({ retailerInfo }) => {
+  const handleMouseDown = (event) => {
+    if (event.button === 1) {
+      // Middle click opens in a new tab
+      window.open(retailerInfo.url, "_blank");
+    }
+  };
+
   return (
-    <div
+    <a
       href={retailerInfo.url}
       target="_blank"
-      className="rounded-md border p-2 w-full bg-white"
+      rel="noopener noreferrer"
+      className="rounded-md border p-2 w-full bg-white block cursor-pointer"
+      //Use for detecting the click on the link
+      onMouseDown={handleMouseDown} 
     >
-      <a className="text-blue-400 cursor-pointer">{retailerInfo.title}</a>
+      <p className="text-blue-400">{retailerInfo.title}</p>
       <h4 className="text-green-600">${retailerInfo.price}</h4>
       <p>
-        Rating: {"⭐".repeat(Math.floor(retailerInfo.rating))} (
-        {retailerInfo.rating})
+        Rating: {"⭐".repeat(Math.floor(retailerInfo.rating))} ({retailerInfo.rating})
       </p>
-    </div>
+    </a>
   );
 };
 
