8e4deac6bc14a0fb6f0c2c79879059aa48889cb1
JohnVVo <3johnvo@gmail.com>
2025-03-23 05:23:26 -0700

    Tailwind css

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,9 +3,12 @@ import React from "react";
 const Card = ({ retailerInfo }) => {
   return (
     <div className="rounded-md border p-2 w-full bg-white">
-      <p>Product: {retailerInfo.title}</p>
-      <p>${retailerInfo.price}</p>
-      <p>Rating: {retailerInfo.rating}/5</p>
+      <p>{retailerInfo.title}</p>
+      <h4 className="text-green-600">${retailerInfo.price}</h4>
+      <p>
+        Rating: {"⭐".repeat(Math.floor(retailerInfo.rating))} (
+        {retailerInfo.rating})
+      </p>
     </div>
   );
 };
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from "react";
+import React, { useState, useRef, useCallback, useEffect } from "react";
 import html2canvas from "html2canvas";
 // Components
 import Webcam from "react-webcam";
@@ -9,20 +9,19 @@ import Card from "../components/Card";
 const MainPage = () => {
   const [image, setImage] = useState(null); // Store the uploaded image file
   const [preview, setPreview] = useState(null); // Store the image preview URL
-  const [labels, setLabels] = useState([]); // Store detected object labels
-  const [logos, setLogos] = useState([]); // Store detected brand logos
-  const [extractedText, setExtractedText] = useState(""); // Store extracted text
-
-  const [searchResults, setSearchResults] = useState(""); // Store extracted text
-  const [aiDesc, setAiDesc] = useState(""); // Store extracted text
-  const [retailerResults, setRetailerResults] = useState([]);
-
-  const [searchQuery, setSearchQuery] = useState("");
+  const [json, setJson] = useState();
   const [toggleMode, setToggleMode] = useState(false);
-
+  const [expand, toggleExpand] = useState(false);
   const [loading, setLoading] = useState(false); // Track loading state
   const webcamRef = useRef(null);
   const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    if (json) {
+      console.log(json.results);
+      console.log(json.ai_description);
+    }
+  }, [json]);
 
   const capturePhoto = useCallback(async () => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -35,12 +34,6 @@ const MainPage = () => {
     console.log(e);
   };
 
-  if (searchResults) {
-    console.log(searchResults);
-  }
-  if (aiDesc) {
-    console.log(aiDesc);
-  }
   // Handle file selection
   const handleUpload = (e) => {
     const file = e.target.files[0];
@@ -68,13 +61,7 @@ const MainPage = () => {
 
       const result = await response.json();
       if (response.ok) {
-        setSearchQuery(result.search_query || "");
-        setLabels(result.labels || []); // Update labels
-        setLogos(result.logos || []); // Update logos
-        setExtractedText(result.text || ""); // Update extracted text
-        setSearchResults(result.results);
-        setAiDesc(result.ai_description);
-        console.log(result.retailer);
+        setJson(result);
       } else {
         alert(result.error || "Failed to analyze image");
       }
@@ -87,7 +74,7 @@ const MainPage = () => {
   };
 
   return (
-    <div className="w-full flex flex-col items-center fancy">
+    <div className={`w-full flex items-center fancy flex-col`}>
       <div className="logo-container">
         <div className="left">
           <h1 className="logo-symbol">🛒</h1>
@@ -97,15 +84,20 @@ const MainPage = () => {
           <h2 className="sublogo">Retail Product Scanner</h2>
         </div>
       </div>
-      <div className="main-container">
-        <div className="row">
-          <div className="box-1">
-            {/* Image Preview */}
-            <div>
-              {preview && <img src={preview} alt="Preview" width="200px" />}
-            </div>
+      <div
+        className={`transition flex place-items-center ${
+          json ? "flex-row gap-2 justify-center" : "flex-col"
+        }`}
+      >
+        <div
+          className={`main-container p-2 place-self-start ${
+            image ? "border rounded-md bg-white" : null
+          }`}
+        >
+          <div className="row">
             <div>
               <button
+                className="mb-2"
                 onClick={() =>
                   !toggleMode ? setToggleMode(true) : setToggleMode(false)
                 }
@@ -115,7 +107,12 @@ const MainPage = () => {
               {/* File Upload */}
               {!toggleMode ? (
                 <>
-                  <input type="file" accept="image/*" onChange={handleUpload} />
+                  <input
+                    type="file"
+                    className="border file:text file:p-2  file:border-r"
+                    accept="image/*"
+                    onChange={handleUpload}
+                  />
                 </>
               ) : (
                 <Camera
@@ -126,6 +123,17 @@ const MainPage = () => {
                   webcamRef={webcamRef}
                 ></Camera>
               )}
+              {/* Image Preview */}
+              <div>
+                {preview && (
+                  <img
+                    src={preview}
+                    alt="Preview"
+                    width="200px"
+                    className="border rounded-full place-self-center"
+                  />
+                )}
+              </div>
               <button
                 className={`rounded-full p-2 border ${
                   loading ? "loading" : ""
@@ -142,48 +150,80 @@ const MainPage = () => {
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Display Results */}
-      <div className="p-5 flex justify-center flex-col items-center">
+        {/* Display Results */}
+        {json && (
+          <div className="p-5 flex justify-center flex-col items-center gap-2 w-1/2 border rounded-md bg-white overflow-y-scroll">
+            {json.search_query && <h1>Searched for "{json.search_query}"</h1>}
 
-      <h3>-------AI Description-------</h3>
-        {aiDesc ? <p>{aiDesc}</p> : null }
+            <div className="w-full bg-white border rounded-md p-2">
+              <h3 className="text-center text-gray-400">
+                AI-Generated Description
+              </h3>
+              {json.ai_description ? <p>{json.ai_description}</p> : null}
+            </div>
+            <p
+              className="w-full text-start text-gray-500 cursor-pointer italic"
+              onClick={() =>
+                expand ? toggleExpand(false) : toggleExpand(true)
+              }
+            >
+              {expand ? "v Collapse" : "> View image search results"}
+            </p>
+            {expand ? (
+              <div className="flex flex-col gap-2">
+                <div className="w-full bg-white border rounded-md p-2">
+                  <h3 className="text-center text-gray-400">Detected Labels</h3>
+                  {json.labels ? (
+                    <ul className="flex gap-2">
+                      {[...new Set(json.labels)].map((label, index) => (
+                        <li key={index} className="rounded-full border p-2">
+                          {label}
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <p>No labels detected</p>
+                  )}
+                </div>
 
-        {searchQuery && <h3>Searched for "{searchQuery}"</h3>}
-        <h3>-------Detected Labels-------</h3>
-        {labels.length > 0 ? (
-          <ul>
-            {labels.map((label, index) => (
-              <li key={index} className="">
-                {label}
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No labels detected</p>
+                <div className="w-full bg-white border rounded-md p-2">
+                  <h3 className="text-center text-gray-400">Detected Logos</h3>
+                  {json.logos ? (
+                    <ul className="flex gap-2">
+                      {[...new Set(json.logos)].map((logo, index) => (
+                        <li key={index} className="rounded-full border p-2">
+                          {logo}
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <p>No logos detected</p>
+                  )}
+                </div>
+
+                <div className="w-full bg-white border rounded-md p-2">
+                  <h3 className="text-center text-gray-400">Extracted Text</h3>
+                  {json.text ? <p>{json.text}</p> : <p>No text detected</p>}
+                </div>
+              </div>
+            ) : null}
+
+            <div className="w-full bg-white border rounded-md p-2">
+              <h3 className="text-center text-gray-400">Search Results</h3>
+              <div className="flex flex-col gap-2">
+                {json.results
+                  ? json.results.map((retailer, index) => (
+                      <Card
+                        key={`retailer-${index}`}
+                        retailerInfo={retailer}
+                      ></Card>
+                    ))
+                  : null}
+              </div>
+            </div>
+          </div>
         )}
-
-        <h3>-------Detected Logos-------</h3>
-        {logos.length > 0 ? (
-          <ul>
-            {logos.map((logo, index) => (
-              <li key={index}>{logo}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No logos detected</p>
-        )}
-
-        <h3>-------Extracted Text-------</h3>
-        {extractedText ? <p>{extractedText}</p> : <p>No text detected</p>}
-
-        <h3>-------Search Results-------</h3>
-        {searchResults
-          ? searchResults.map((retailer, index) => (
-              <Card key={`retailer-${index}`} retailerInfo={retailer}></Card>
-            ))
-          : null}
       </div>
     </div>
   );
